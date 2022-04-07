ec0b9a012bccecd6aba743fda015c633bc39ff3f
Daniil <mirsh.work@gmail.com>
2022-04-07 18:49:46 +0600

    stopwatch created

---
+++ gulp/config/ftp.js
@@ -0,0 +1,6 @@
+export let configFTP = {
+	host: "#",
+	user: "Dan",
+	password: "dm276857922",
+	parallel: 5,
+}
---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+import * as ezFunctions from "./modules/functions.js";
+
+ezFunctions.isWebp();
+
+/*import Swiper, { Navigation, Pagination } from 'swiper';
+
+const swiper = new Swiper();*/
+
+// Свой скрипт ===================================================================================================================
+import "./files/script.js";
